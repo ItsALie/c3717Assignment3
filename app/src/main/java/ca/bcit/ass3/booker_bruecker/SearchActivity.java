e8485316062ffe1b5c11902435d7c369c126f8e9
CanOSpam <brooker_113@hotmail.com>
2017-11-11 17:52:20 -0800

    Search works, but DB query needs a fix

--- app/src/main/java/ca/bcit/ass3/booker_bruecker/SearchActivity.java
+++ app/src/main/java/ca/bcit/ass3/booker_bruecker/SearchActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,10 +18,20 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            System.out.println("searching for: " + query);
+        ListView listOfEvents = (ListView) findViewById(R.id.list_events);
+        helper = new myDbAdapter(this);
+
+        String searchQuery = getIntent().getStringExtra(SearchManager.QUERY);
+
+        final List<String> itemList = Arrays.asList(helper.getNameSearch(searchQuery).split("\n"));
+
+        if(helper.getNameSearch(searchQuery).length() != 0) {
+            myCustomAdapter adapter = new myCustomAdapter(itemList, this);
+            listOfEvents.setAdapter(adapter);
+        } else {
+            Toast.makeText(getApplicationContext(), "No results found", Toast.LENGTH_LONG).show();
         }
+
+
     }
 }
